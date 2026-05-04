67402c4cf592d3bbf8ec156d99f9244a934028c9
KRr <braddakingjr@gmail.com>
2026-05-04 18:38:58 -0400

    2 prong apporach for feautre extraction. going to be building out helper method first which pulls from the file path location for each and goes and does the sumary stats for all of the ones we are supposed to do. now that I have the first one going to copy and paste it down

--- SER_Project/minilearn/featureextraction.py
+++ SER_Project/minilearn/featureextraction.py
@@ -8,11 +8,31 @@ from tqdm import tqdm
 #as seen in the example
 SAMPLE_RATE = 48000
 
-
+#hopefully WAV_path lines up to CSV/dataframe
 def extract_features(wav_path):
     """Load one wav file and return a dict of its features."""
-
-    y, sr = librosa.load(str(wav_path), sr=SAMPLE_RATE)
+    try:
+        y, sr = librosa.load(str(wav_path), sr=SAMPLE_RATE)
+    except Exception as e:
+        print("Skipping bad file")
+        return None
+        
+    #same thing like with rows when appending last time    
     features = {}
 
+    #MFCC 13 coefficents from notes
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    for i in range(13):
+        features[f"mfcc_{i+1}_mean"] = mfcc[i].mean()
+        features[f"mfcc_{i+1}_std"]  = mfcc[i].std()
+        features[f"mfcc_{i+1}_min"]  = mfcc[i].min()
+        features[f"mfcc_{i+1}_max"]  = mfcc[i].max()
+    
+    #MFCC delta
+    mfcc_delta = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    for i in range(13):
+        features[f"mfcc_{i+1}_mean"] = mfcc[i].mean()
+        features[f"mfcc_{i+1}_std"]  = mfcc[i].std()
+        features[f"mfcc_{i+1}_min"]  = mfcc[i].min()
+        features[f"mfcc_{i+1}_max"]  = mfcc[i].max()
    
