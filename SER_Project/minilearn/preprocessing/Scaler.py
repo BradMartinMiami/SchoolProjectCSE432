ee8990704b2ecc81da70f805a18cccc4c8a315b0
KRr <braddakingjr@gmail.com>
2026-05-04 23:28:29 -0400

    The scaler is extremely simple at this point just makes arrays to calculate down across and saves the z score normalized value. looking at sickit-learn api there seems to be alot of functions that goes into it so will have to come back later

--- SER_Project/minilearn/preprocessing/Scaler.py
+++ SER_Project/minilearn/preprocessing/Scaler.py
@@ -1,2 +1,25 @@
 import numpy as np
 
+class StandardScaler:
+    #basic constructor when called with two variables
+    def _init_(self):
+        self.mean = None
+        self.std = None
+
+    #this method saves the mean and std value to be used by the transform
+    def fit(self, X):
+        #makes an array of the values
+        X = np.array(X, dtype=float)
+        #calculates down the row
+        self.mean = np.mean(X, axis=0)
+        self.std = np.std(X, axis=0)
+
+        return self
+    
+    #basically using the saved mean and std to be able to standardized the data
+    def transform(self, X):
+        X = np.array(X, dtype=float)
+
+        X_scaled = (X - self.mean) / self.std
+
+        return X_scaled
