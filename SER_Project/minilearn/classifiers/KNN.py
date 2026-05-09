aad70680f07d9d708d9acaeaccf1c210c0980439
KRr <braddakingjr@gmail.com>
2026-05-08 22:06:54 -0400

    adding the predict for KNN, basically a for loop going through points and then finding the points around it and then classifying what it is by the amount of predictors around it

--- SER_Project/minilearn/classifiers/KNN.py
+++ SER_Project/minilearn/classifiers/KNN.py
@@ -15,3 +15,27 @@ class KNNClassifierMM:
         self.X = np.array(X, dtype=float)
         self.y = np.array(y)
         self.classes = np.unique(self.y)
+
+    def predict(self, X):
+        X = np.array(X, dtype=float)
+        predictions = []
+
+        for i in range (X.shape[0]):
+            first = X[i]
+
+            difference = self.X - first
+            squared = difference ** 2
+            distance = np.sqrt(squared.sum(axis=1))
+
+            sort = np.argsort(distance)
+            nearest = sort[:self.k]
+
+            neighbors_class = self.y[nearest]
+        
+            values, counts = np.unique(neighbors_class, return_counts=True)
+            winner = values[np.argmax(counts)]
+            predictions.append(winner)
+        
+        return np.array(predictions)
+    
+    
