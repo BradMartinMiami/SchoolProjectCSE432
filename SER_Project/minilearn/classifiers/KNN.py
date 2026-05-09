5767491c03b03031b3e23928cfc93d95b174889d
KRr <braddakingjr@gmail.com>
2026-05-08 22:28:43 -0400

    did the predict proba and the score for KNN and now going to use it against the sickit learn model

--- SER_Project/minilearn/classifiers/KNN.py
+++ SER_Project/minilearn/classifiers/KNN.py
@@ -38,4 +38,36 @@ class KNNClassifierMM:
         
         return np.array(predictions)
     
+
+    def predict_proba(self, X):
+        # for ROC curves we need a probability per class
+        # for kNN we just use the fraction of the k neighbors that are each class
+        X = np.array(X, dtype=float)
+        n_test = X.shape[0]
+        n_classes = len(self.classes)
+        probs = np.zeros((n_test, n_classes))
+ 
+        for i in range(n_test):
+            test_point = X[i]
+ 
+            # same distance calculation as predict()
+            diffs = self.X - test_point
+            dists = np.sqrt((diffs ** 2).sum(axis=1))
+ 
+            sorted_idx = np.argsort(dists)
+            nearest = sorted_idx[:self.k]
+            neighbor_labels = self.y[nearest]
+ 
+            # for each class, what fraction of the k neighbors are that class
+            for j in range(n_classes):
+                probs[i, j] = np.mean(neighbor_labels == self.classes[j])
+ 
+        return probs
+    
+    def score(self, X, y):
+        preds = self.predict(X)
+        y = np.array(y)
+        return np.mean(preds == y)
+    
+
     
